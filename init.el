aa7aaaf1456c327a873795d6309b92134e0ba414
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-08-28 08:07:11 -0300

    Fix window layout for Emacs 25
    While this function worked perfectly on Emacs 24, it switched the main
    buffer into the wrong window on Emacs 25 (super annoying).
    
    Now it should be fixed, but it will probably break on Emacs 24...

--- init.el
+++ init.el
@@ -440,7 +440,7 @@
   (other-window (- 1))
   (split-window-vertically)
   (shell)
-  (other-window 1)
+  (other-window -1)
   (switch-to-buffer "*scratch*")
   (other-window (- 1)))
 (global-set-key (kbd "C-c w") 'default-layout)
