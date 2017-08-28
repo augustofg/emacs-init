a5bbcebb971142e7244e1ddb59ef0fce070f2a4c
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-08-28 09:20:30 -0300

    Add some packages to installpkg.el
    Rust, Julia and Markdown stuff...

--- installpkg.el
+++ installpkg.el
@@ -26,7 +26,7 @@
   "Install some neat packages"
   (interactive)
   (let (package-list)
-	(setq package-list '(w3m magit auctex ace-jump-mode irony company-irony company-jedi linum-relative yasnippet waher-theme google-translate use-package))
+	(setq package-list '(w3m magit auctex ace-jump-mode irony company-irony company-jedi linum-relative yasnippet waher-theme google-translate use-package rust-mode flycheck-rust company-racer racer julia-mode julia-repl markdown-mode))
 	(package-refresh-contents)
 	(package-initialize)
 	(dolist (package package-list)
