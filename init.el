984803e26c19b0a0ddadb6d0a7cadce0e41bfd4b
Augusto Fraga Giachero <augustofg96@gmail.com>
2017-08-28 09:46:30 -0300

    Fix some coments and style
    The fullscreen fix is not because KDE doesn't handle the F11 key, it
    is because Emacs prior 24.4 version doesn't handle it.

--- init.el
+++ init.el
@@ -18,29 +18,26 @@
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 
-;; KDE full screen fix ;;
-
 ;; If your emacs version is < 24.4
-
 ;; (defun my-fullscreen ()
 ;;   (interactive)
 ;;   (set-frame-parameter nil 'fullscreen 'fullboth) this makes the frame go fullscreen
 ;;   (tool-bar-mode -1) these 3 lines turn off GUI junk
 ;;   (scroll-bar-mode -1)
 ;;   (menu-bar-mode -1))
-
+;;
 ;; (defun my-non-fullscreen ()
 ;;   (interactive)
 ;;   (set-frame-parameter nil 'width 82)
 ;;   (set-frame-parameter nil 'fullscreen 'fullheight)
 ;;   (menu-bar-mode t)) I don't turn tool-bar and scroll-bar back on b/c I never want them
-
+;;
 ;; (defun toggle-fullscreen ()
 ;;   (interactive)
 ;;   (if (eq (frame-parameter nil 'fullscreen) 'fullboth)  tests if already fullscreened
 ;;       (my-non-fullscreen)
 ;;     (my-fullscreen)))
-
+;;
 ;; (global-set-key (kbd "<f11>") 'toggle-fullscreen)
 
 ;;;;
